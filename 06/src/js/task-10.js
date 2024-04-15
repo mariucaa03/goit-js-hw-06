af55ebfd762b6ef8816197294daec6fcd49cda87
mariuca <zahariuc_mariuca@yahoo.com>
2024-04-15 22:43:53 +0300

    Update task-10.js

--- 06/src/js/task-10.js
+++ 06/src/js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.getElementById("controls");
+const boxes = document.getElementById("boxes");
+
+controls.addEventListener("click", (event) => {
+if (event.target.dataset.create) {
+const input = controls.querySelector("input");
+const amount = parseInt(input.value);
+createBoxes(amount);
+}
+
+if (event.target.dataset.destroy) {
+destroyBoxes();
+}
+});
+
+function createBoxes(amount) {
+for (let i = 0; i < amount; i++) {
+const box = document.createElement("div");
+box.style.width = "${30 + i * 10}px";
+box.style.height = "${30 + i * 10}px";
+box.style.backgroundColor = getRandomHexColor();
+boxes.appendChild(box);
+}
+}
+
+function destroyBoxes() {
+boxes.innerHTML = "";
+}
+
